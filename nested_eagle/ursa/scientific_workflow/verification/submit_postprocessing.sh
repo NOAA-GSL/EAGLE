8de050a23c0dbadccdb488e4d3bed64b55e7855c
D. Alex Burrows <David.Burrows@noaa.gov>
2025-11-06 14:00:05 +0000

    merge with main x2

--- nested_eagle/ursa/scientific_workflow/verification/submit_postprocessing.sh
+++ nested_eagle/ursa/scientific_workflow/verification/submit_postprocessing.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-<<<<<<<< HEAD:nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
-#SBATCH -J nested_eagle_verification 
-#SBATCH -o slurm/verification.%j.out
-#SBATCH -e slurm/verification.%j.err
-========
-#SBATCH -J perform_postprocessing
-#SBATCH -o slurm-%j.out
-#SBATCH -e slurm-%j.err
->>>>>>>> 8a4c16f2046b03ca8ec5cc53b4e9a2eabc4dfdae:nested_eagle/ursa/scientific_workflow/verification/submit_postprocessing.sh
+#SBATCH -J nested_eagle_postprocessing 
+#SBATCH -o slurm/postprocessing.%j.out
+#SBATCH -e slurm/postprocessing.%j.err
 #SBATCH --account=epic
 #SBATCH --partition=u1-service
 #SBATCH --mem=128g
@@ -17,9 +11,8 @@
 #SBATCH --ntasks=1
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
+conda activate eagle
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-conda activate eagle 
 
 python postprocess.py
--- nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
+++ nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-<<<<<<< HEAD:nested_eagle/ursa/scientific_workflow/validation/submit_verification.sh
-<<<<<<<< HEAD:nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
 #SBATCH -J nested_eagle_verification 
 #SBATCH -o slurm/verification.%j.out
 #SBATCH -e slurm/verification.%j.err
-========
-#SBATCH -J perform_postprocessing
-#SBATCH -o slurm-%j.out
-#SBATCH -e slurm-%j.err
->>>>>>>> 8a4c16f2046b03ca8ec5cc53b4e9a2eabc4dfdae:nested_eagle/ursa/scientific_workflow/verification/submit_postprocessing.sh
-=======
-#SBATCH -J perform_verification 
-#SBATCH -o slurm-%j.out
-#SBATCH -e slurm-%j.err
->>>>>>> 8a4c16f2046b03ca8ec5cc53b4e9a2eabc4dfdae:nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
 #SBATCH --account=epic
 #SBATCH --partition=u1-service
 #SBATCH --mem=128g
@@ -23,23 +11,14 @@
 #SBATCH --ntasks=1
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
+conda activate wxvx
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-<<<<<<< HEAD:nested_eagle/ursa/scientific_workflow/validation/submit_verification.sh
-conda activate eagle 
-
-python postprocess.py
-=======
 export WORKDIR_BASE_PATH=$PWD
 
 sed -i "/^.*workdir:.*$/c\  workdir: $WORKDIR_BASE_PATH\/wxvx_workdir\/lam" wxvx_lam.yaml
-
 sed -i "/^.*workdir:.*$/c\  workdir: $WORKDIR_BASE_PATH\/wxvx_workdir\/global" wxvx_global.yaml
-
-conda activate wxvx 
 
 wxvx -c wxvx_lam.yaml -t plots
 
 wxvx -c wxvx_global.yaml -t plots
->>>>>>> 8a4c16f2046b03ca8ec5cc53b4e9a2eabc4dfdae:nested_eagle/ursa/scientific_workflow/verification/submit_verification.sh
